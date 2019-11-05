e9a64b5676d943bca40e93e849fc7daa057a4432
Alicia Yu <aliciayu@v1040-wn-rt-a-68-70.campus-dynamic.uwaterloo.ca>
2019-11-05 18:20:32 -0500

    footer

---
+++ src/Components/Footer/Footer.jsx
@@ -0,0 +1,15 @@
+import React, {Component} from 'react';
+
+class Footer extends Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render(){
+        return(
+            <div className="footer_main">
+                <p>this is a footer</p>
+            </div>
+        )
+    }
+}
