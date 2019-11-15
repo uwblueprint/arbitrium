d11d3ba0b42ff7c912e215b03d11011ad3d25b98
Sherry Li <sherryhli@users.noreply.github.com>
2019-11-14 21:59:34 -0500

    Add initial backend set-up

---
+++ backend/seed.js
@@ -0,0 +1,122 @@
+const REVIEWS = [
+    {
+        userId: 1,
+        appId: 1,
+        isGlobal: false,
+        rating:  3,
+        progress: 0,
+        rank: 3,
+        lastReviewed: '2019-11-14T03:24:00',
+        questionRatings: [
+            {
+                questionKey: 1,
+                rating: ''
+            },
+            {
+                questionKey: 2,
+                rating: ''
+            }
+        ],
+        comments: ''
+    },
+    {
+        userId: 1,
+        appId: 2,
+        isGlobal: false,
+        rating:  4,
+        progress: 0,
+        rank: 2,
+        lastReviewed: '2019-11-13T03:24:00',
+        questionRatings: [
+            {
+                questionKey: 1,
+                rating: ''
+            },
+            {
+                questionKey: 2,
+                rating: ''
+            }
+        ],
+        comments: ''
+    },
+    {
+        userId: 1,
+        appId: 3,
+        isGlobal: false,
+        rating:  5,
+        progress: 0,
+        rank: 1,
+        lastReviewed: '2019-11-13T05:23:00',
+        questionRatings: [
+            {
+                questionKey: 1,
+                rating: ''
+            },
+            {
+                questionKey: 2,
+                rating: ''
+            }
+        ],
+    },
+    {
+        userId: 2,
+        appId: 1,
+        isGlobal: false,
+        rating:  3,
+        progress: 0,
+        rank: 3,
+        lastReviewed: '2019-11-14T03:24:00',
+        questionRatings: [
+            {
+                questionKey: 1,
+                rating: ''
+            },
+            {
+                questionKey: 2,
+                rating: ''
+            }
+        ],
+        comments: ''
+    },
+    {
+        userId: 2,
+        appId: 2,
+        isGlobal: false,
+        rating:  4,
+        progress: 0,
+        rank: 2,
+        lastReviewed: '2019-11-13T03:24:00',
+        questionRatings: [
+            {
+                questionKey: 1,
+                rating: ''
+            },
+            {
+                questionKey: 2,
+                rating: ''
+            }
+        ],
+        comments: ''
+    },
+    {
+        userId: 2,
+        appId: 3,
+        isGlobal: false,
+        rating:  5,
+        progress: 0,
+        rank: 1,
+        lastReviewed: '2019-11-13T03:24:00',
+        questionRatings: [
+            {
+                questionKey: 1,
+                rating: ''
+            },
+            {
+                questionKey: 2,
+                rating: ''
+            }
+        ],
+    }
+];
+
+module.exports = REVIEWS;
