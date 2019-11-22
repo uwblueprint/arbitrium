bada8dd403da6a3f49e2d07b3df356ca609c0749
Leander Rodrigues <me@leander.xyz>
2019-11-21 21:09:42 -0500

    :clown_face: prelim mock on flow selector

--- src/Components/Application/Application.jsx
+++ src/Components/Application/Application.jsx
@@ -1,7 +1,22 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
+import styled from "styled-components";
+import FlowSelector from "../FlowSelector/FlowSelector";
 
+const Wrapper = styled.div`
+  margin: 0 auto;
+  max-width: 800px;
+  border: 2px solid red;
+`;
 export default class Application extends Component {
   render() {
-    return <div>Application</div>
+    return (
+      <>
+        <FlowSelector>
+          <button>1. Letter of Interest</button>
+          <button disabled>2. Full Application</button>
+        </FlowSelector>
+        <Wrapper></Wrapper>
+      </>
+    );
   }
 }
