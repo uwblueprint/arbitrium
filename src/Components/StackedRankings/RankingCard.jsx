c52f7fc4122fcdb1d913ce63a7aedcae0150b308
hyunzeekim <hyunzeekim01@gmail.com>
2020-01-16 22:30:24 -0500

    stacked rankings

--- src/Components/StackedRankings/RankingCard.jsx
+++ src/Components/StackedRankings/RankingCard.jsx
@@ -1,62 +1,61 @@
-import React from 'react';
+import React from "react";
 import styled from "styled-components";
 
 import Card from "@material-ui/core/Card";
-import CardContent from "@material-ui/core/CardContent"
-import { makeStyles } from '@material-ui/core';
+import CardContent from "@material-ui/core/CardContent";
+import { makeStyles } from "@material-ui/core";
 
 const useStyles = makeStyles({
-    
-    root: {
-        marginLeft: "168px",
-        maxWidth: 800,
-        minHeight: 56,
-        borderRadius: 5,
-        boxShadow: "1px 1px 2px 1px #cccccc",
-        marginBottom: 12
-    },
+  root: {
+    marginLeft: "168px",
+    maxWidth: 800,
+    minHeight: 56,
+    borderRadius: 5,
+    boxShadow: "1px 1px 2px 1px #cccccc",
+    marginBottom: 12
+  },
 
-    company: {
-        float: "left",
-        marginLeft: "64px",
-        color: "#000",
-        fontFamily: "Roboto",
-        lineHeight: "20px",
-        fontSize: "14px",
-        fontWeight: "normal"
-      },
-    
-    rating: {
-        float: "left",
-        marginLeft: "213px",
-        color: "#000",
-        fontFamily: "Roboto",
-        lineHeight: "20px",
-        fontSize: "14px",
-        fontWeight: "normal"
-      },
-    
-    link: {
-        float: "right",
-        color: "#000",
-        fontFamily: "Roboto",
-        lineHeight: "20px",
-        fontSize: "14px",
-        fontWeight: "normal"
-    }
-    
-    });
+  company: {
+    float: "left",
+    marginLeft: "64px",
+    color: "#000",
+    fontFamily: "Roboto",
+    lineHeight: "20px",
+    fontSize: "14px",
+    fontWeight: "normal"
+  },
+
+  rating: {
+    float: "left",
+    marginLeft: "213px",
+    color: "#000",
+    fontFamily: "Roboto",
+    lineHeight: "20px",
+    fontSize: "14px",
+    fontWeight: "normal"
+  },
+
+  link: {
+    float: "right",
+    color: "#000",
+    fontFamily: "Roboto",
+    lineHeight: "20px",
+    fontSize: "14px",
+    fontWeight: "normal"
+  }
+});
 
 function RankingCard() {
-    const classes = useStyles();
-    return (
-        <Card className={classes.root}>
-            <CardContent>
-                <div className={classes.company}>a company</div>
-                <div className={classes.rating}>Your Rating: 5/5</div>
-                <div className={classes.link}>link here</div>
-            </CardContent>
-        </Card>);
+  const classes = useStyles();
+  return (
+    <Card className={classes.root}>
+      <CardContent>
+        <div className={classes.company}>a company</div>
+        <div className={classes.rating}>Your Rating: 5/5</div>
+        <div className={classes.link}>link here</div>
+      </CardContent>
+    </Card>
+  );
 }
 
 export default RankingCard;
--- src/Components/StackedRankings/StackedRankings.jsx
+++ src/Components/StackedRankings/StackedRankings.jsx
@@ -4,7 +4,11 @@ import styled from "styled-components";
 import RankingCard from "./RankingCard"
 
 const Wrapper = styled.div`
+<<<<<<< HEAD
     margin-top: 148px
+=======
+    margin-top: 200px
+>>>>>>> stacked rankings
 `
 
 function StackedRankings() {
