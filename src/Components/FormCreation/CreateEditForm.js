30e80affc1dd7fbf8137c5b80c82f46f9dbfccca
Xin Hao Zhang <xinhao.zhang98@gmail.com>
2020-08-20 19:27:00 -0400

    Custom forms state management

--- src/Components/FormCreation/CreateEditForm.js
+++ src/Components/FormCreation/CreateEditForm.js
@@ -1,84 +1,32 @@
-import React, { useReducer, useCallback } from "react";
+import React, { useReducer } from "react";
 import { HEADER_HEIGHT } from "../Header/Header";
-import InputBase from "@material-ui/core/InputBase";
-import customFormStateReducer, {
-  EDIT_TITLE,
-  EDIT_DESCRIPTION
-} from "../../Reducers/CustomFormStateReducer";
 import styled from "styled-components";
+
+import CreateEditFormHeader from "./CreateEditFormHeader";
+import {
+  CreateEditFormDispatchContext,
+  CreateEditFormStateContext,
+  defaultFormState
+} from "./CreateEditFormStateManagement";
+import customFormStateReducer from "../../Reducers/CustomFormStateReducer";
 
 const Wrapper = styled.div`
   margin-top: ${HEADER_HEIGHT}px;
 `;
 
-const Header = styled.div`
-  padding: 48px 100px;
-  box-sizing: border-box;
-  height: 176px;
-  box-shadow: 0 2px 3px 1px #cccccc;
-`;
-
-const TitleInput = styled(InputBase)`
-  input {
-    font-size: 24px;
-  }
-  && {
-    margin-bottom: 16px;
-    line-height: 36px;
-  }
-`;
-
-const DescriptionInput = styled(InputBase)`
-  && {
-    width: 646px
-    display: block;
-    line-height: 21px;
-    overflow-y: auto;
-    max-height: 48px;
-  }
-  textarea {
-    font-size: 14px;
-    color: #888888;
-  }
-`;
-
-const defaultForm = {
-  title: "Untitled Form",
-  description: ""
-};
-
 function CreateEditForm() {
-  const [formState, dispatch] = useReducer(customFormStateReducer, defaultForm);
-
-  const onTitleChange = useCallback(
-    (e) => {
-      dispatch({ type: EDIT_TITLE, title: e.target.value });
-    },
-    [dispatch]
-  );
-
-  const onDescriptionChange = useCallback(
-    (e) => {
-      dispatch({ type: EDIT_DESCRIPTION, description: e.target.value });
-    },
-    [dispatch]
+  const [formState, dispatch] = useReducer(
+    customFormStateReducer,
+    defaultFormState
   );
 
   return (
     <Wrapper>
-      <Header>
-        <TitleInput
-          placeholder="Form Title"
-          value={formState.title}
-          onChange={onTitleChange}
-        />
-        <DescriptionInput
-          multiline
-          placeholder="Form description..."
-          value={formState.description}
-          onChange={onDescriptionChange}
-        />
-      </Header>
+      <CreateEditFormStateContext.Provider value={formState}>
+        <CreateEditFormDispatchContext.Provider value={dispatch}>
+          <CreateEditFormHeader />
+        </CreateEditFormDispatchContext.Provider>
+      </CreateEditFormStateContext.Provider>
     </Wrapper>
   );
 }
