a022ba73176fc39ec2dca74dcb50ceec2aac0410
Nyle Kabiruddin Dharani <nkdharani@uwaterloo.ca>
2020-08-21 19:46:08 -0400

    lint routes

--- backend/routes/feedback.js
+++ backend/routes/feedback.js
@@ -1,32 +1,31 @@
 const express = require("express");
-
+var mongoose = require("mongoose");
 const router = express.Router();
 const db = require("../mongo.js");
-
-const Feedback = require('../models/feedback')
+const Feedback = require("../models/feedback");
 
 router.post("/", function(req, res) {
-
   const feedback = new Feedback({
     _id: new mongoose.Types.ObjectId(),
-    userId: req.body.userId, 
+    userId: req.body.userId,
     expierence: req.body.expierence,
     feedbackPar: req.boy.feedbackPar,
     comment: req.boy.comment
   });
 
-  feedback.save()
-          .then(result => {
-            console.log(result); 
-            res.status(201).json({
-              message: "POST request to /feedback",
-              createdFeedback: feedback
-            }); 
-          })
-          .catch(err => {
-            console.log(err); 
-            res.send(err);
-          }); 
-  });
+  feedback
+    .save()
+    .then((result) => {
+      console.log(result);
+      res.status(201).json({
+        message: "POST request to /feedback",
+        createdFeedback: feedback
+      });
+    })
+    .catch((err) => {
+      console.log(err);
+      res.send(err);
+    });
+});
 
 module.exports = router;
