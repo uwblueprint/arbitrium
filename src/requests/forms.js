b3b64f71467d63a9d479a3293ed9aa28eda1ee03
PRANNOY LAL <prannoylal@gmail.com>
2020-10-08 20:54:53 -0400

    fixed prettier issues

--- src/requests/forms.js
+++ src/requests/forms.js
@@ -1,55 +1,54 @@
 import { GET, POST, PATCH, DELETE } from "./Helper.js";
 
-
 // FORMS
-
 async function createForm(databody) {
-    return POST("/api/forms", databody);
+  return POST("/api/forms", databody);
 }
 
 async function getForm(formId) {
-    return GET(`api/forms/${formId}`);
+  return GET(`api/forms/${formId}`);
 }
 
 async function deleteForm(formId) {
-    return DELETE(`api/forms/${formId}`);
+  return DELETE(`api/forms/${formId}`);
 }
-
 
 // SECTIONS
 async function createSection(formId, databody) {
-    return POST(`api/forms/${formId}/sections`, databody);
+  return POST(`api/forms/${formId}/sections`, databody);
 }
 
 async function deleteSection(formId, sectionId) {
-    return DELETE(`api/forms/${formId}/sections/${sectionId}`);
+  return DELETE(`api/forms/${formId}/sections/${sectionId}`);
 }
 
 async function updateSection(formId, databody) {
-    return PATCH(`api/forms/${formId}/sections`, databody);
+  return PATCH(`api/forms/${formId}/sections`, databody);
 }
 
 // QUESTIONS
 async function createQuestion(formId, sectionId, databody) {
-    return POST(`api/forms/${formId}/sections/${sectionId}/questions`, databody);
+  return POST(`api/forms/${formId}/sections/${sectionId}/questions`, databody);
 }
 
 async function deleteQuestion(formId, sectionId, questionId) {
-    return DELETE(`api/forms/${formId}/sections/${sectionId}/questions/${questionId}`);
+  return DELETE(
+    `api/forms/${formId}/sections/${sectionId}/questions/${questionId}`
+  );
 }
 
 async function updateQuestion(formId, sectionId, databody) {
-    return PATCH(`api/forms/${formId}/sections/${sectionId}/questions`, databody);
+  return PATCH(`api/forms/${formId}/sections/${sectionId}/questions`, databody);
 }
 
 export {
-   getForm,
-   createForm,
-   deleteForm,
-   createSection,
-   deleteSection,
-   updateSection,
-   createQuestion,
-   deleteQuestion,
-   updateQuestion
-  };
+  getForm,
+  createForm,
+  deleteForm,
+  createSection,
+  deleteSection,
+  updateSection,
+  createQuestion,
+  deleteQuestion,
+  updateQuestion
+};
