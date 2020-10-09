b8069aa6ea7a77ece265f6f723b11e1fcdefc1a7
Guy Romm <guy.romm@outlook.com>
2020-10-08 20:40:23 -0400

    Removed unused var

--- src/requests/delete.js
+++ src/requests/delete.js
@@ -1,13 +1,5 @@
 import { DELETE } from "./Helper";
 
-let proxy = "http://localhost:4000";
-if (process.env.REACT_APP_NODE_ENV === "production") {
-  proxy = process.env.REACT_APP_SERVER_PROD;
-}
-if (process.env.REACT_APP_NODE_ENV === "qa") {
-  proxy = process.env.REACT_APP_SERVER_QA;
-}
-
 async function deleteUserAPI(userId) {
   DELETE(`/api/users/${userId}`);
 }
