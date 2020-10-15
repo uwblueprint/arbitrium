61c643c39a85f2a7c2426d31cbdc19148be6726c
ahmedhamodi <ahmed.r.hamodi@gmail.com>
2020-10-15 19:21:24 -0400

    Properly load form data.

--- src/requests/forms.js
+++ src/requests/forms.js
@@ -6,39 +6,45 @@ async function createForm(databody) {
 }
 
 async function getForm(formId) {
-  return GET(`api/forms/${formId}`);
+  return GET(`/api/forms/${formId.formId}`);
 }
 
 async function deleteForm(formId) {
-  return DELETE(`api/forms/${formId}`);
+  return DELETE(`/api/forms/${formId.formId}`);
 }
 
 // SECTIONS
 async function createSection(formId, databody) {
-  return POST(`api/forms/${formId}/sections`, databody);
+  return POST(`/api/forms/${formId.formId}/sections`, databody);
 }
 
 async function deleteSection(formId, sectionId) {
-  return DELETE(`api/forms/${formId}/sections/${sectionId}`);
+  return DELETE(`/api/forms/${formId.formId}/sections/${sectionId}`);
 }
 
 async function updateSection(formId, databody) {
-  return PATCH(`api/forms/${formId}/sections`, databody);
+  return PATCH(`/api/forms/${formId.formId}/sections`, databody);
 }
 
 // QUESTIONS
 async function createQuestion(formId, sectionId, databody) {
-  return POST(`api/forms/${formId}/sections/${sectionId}/questions`, databody);
+  return POST(
+    `/api/forms/${formId.formId}/sections/${sectionId}/questions`,
+    databody
+  );
 }
 
 async function deleteQuestion(formId, sectionId, questionId) {
   return DELETE(
-    `api/forms/${formId}/sections/${sectionId}/questions/${questionId}`
+    `/api/forms/${formId.formId}/sections/${sectionId}/questions/${questionId}`
   );
 }
 
 async function updateQuestion(formId, sectionId, databody) {
-  return PATCH(`api/forms/${formId}/sections/${sectionId}/questions`, databody);
+  return PATCH(
+    `/api/forms/${formId.formId}/sections/${sectionId}/questions`,
+    databody
+  );
 }
 
 export {
