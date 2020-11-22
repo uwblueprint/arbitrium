8a69baeec1c8b16856b34146a2d9f39a60ac92d5
Greg Maxin <gmaxin@uwaterloo.ca>
2020-11-22 15:21:42 -0500

    Updated routes for programs

--- backend/routes/programs.js
+++ backend/routes/programs.js
@@ -3,6 +3,7 @@ const express = require("express");
 //Allows api routes to be posted
 const firebaseAdmin = require("../firebaseAdmin");
 const router = express.Router();
+
 //Database connections: returns object of connections (connections["item"])
 const db = require("../mongo.js");
 const addConnection = require("../mongo.js").addConnection;
@@ -25,21 +26,25 @@ router.get("/all", function(req, res) {
     });
 });
 
+//Create a program / Update if already exists
+//DatabaseName is unique
+
+//This route will be used for all update actions for a program.
+//Inlcuding Deletes, which will always be soft
 router.post("/", function(req, res) {
   db["Authentication"].programs
     .updateOne(
       {
-        databaseName: req.body.databaseName,
-        displayName: req.body.displayName
+        databaseName: req.body.databaseName
       },
       req.body,
       { upsert: true }
     )
     // status code 201 means created
-    .then(function(newSchedule) {
+    .then(function(result) {
       //This is a helper function in mongo.js that adds another connection
       addConnection(req.body.databaseName);
-      res.status(201).json(newSchedule);
+      res.status(201).json(result);
     })
     .catch(function(err) {
       res.send(err);
