25fd34cdca194ab3e7c6c4689be8e820d2eba82f
prannoy lal <prannoylal@gmail.com>
2020-12-02 01:51:21 -0500

    basic setup of submission/answer models (#283)
    * basic setup of models
    
    * addressed pr comments
    
    fixed typo
    
    removed import
    
    * addressed bartney's PR comments
    
    * removed options + added identifier

---
+++ backend/models/submission.js
@@ -0,0 +1,71 @@
+const mongoose = require("mongoose");
+
+const coordinates = new mongoose.Schema({
+  x: {
+    type: Number
+  },
+  y: {
+    type: Number
+  }
+});
+
+const answer = new mongoose.Schema({
+  type: {
+    type: String,
+    enum: [
+      "SHORT_ANSWER", //Yoptions.length = 0 | Xoptions.length = 1
+      "PARAGRAPHS", //Yoptions.length = 0 | Xoptions.length = 1
+      "MULTIPLE_CHOICE", //Yoptions.length = # of MC | Xoptions.length = 1
+      "CHECKBOXES", //Yoptions.length = # of boxes | Xoptions.length = 1
+      "FILE_UPLOAD", //Yoptions.length = 0 | Xoptions.length = 1
+      "CHECKBOX_GRID" //(Y|X)options.length = # of choices
+    ]
+  },
+  // SHORT_ANSWER
+  // PARAGRAPH
+  answerString: {
+    type: String
+  },
+  // MULTIPLE_CHOICE
+  // CHECKBOXES
+  answerArray: {
+    type: [String]
+  },
+  // CHECKBOX_GRID
+  answerMatrix: {
+    type: [coordinates]
+  },
+  sectionId: {
+    type: String
+  },
+  questionId: {
+    type: String
+  }
+});
+
+const submissionsSchema = new mongoose.Schema(
+  {
+    formId: {
+      type: String
+    },
+    linkId: {
+      type: String
+    },
+    submissionDate: {
+      type: Date,
+      default: null // If null then it hasn't been submitted (i.e it is in draft mode)
+    },
+    lastSaveDate: {
+      type: Date
+    },
+    answers: {
+      type: [answer]
+    },
+    identifier: {
+      type: String // Since it is a short answer question
+    }
+  },
+  { collection: "Submissions" }
+);
+
+module.exports = submissionsSchema;
