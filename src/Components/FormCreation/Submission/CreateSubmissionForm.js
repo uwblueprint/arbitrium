14dc79f370df6886067c5afe38a59b2c979e3b38
Ahmed Hamodi <ahmed.r.hamodi@gmail.com>
2020-12-03 21:29:34 -0500

    Basic page navigation of the form for submission. (#287)
    * Basic page navigation of the form for submission.
    
    * Fix form section.

--- src/Components/FormCreation/Submission/CreateSubmissionForm.js
+++ src/Components/FormCreation/Submission/CreateSubmissionForm.js
@@ -7,6 +7,7 @@ import usePromise from "../../../Hooks/usePromise";
 import CreateEditFormHeader from "./SubmissionFormHeader";
 import { defaultFormState } from "./SubmissionFormStateManagement";
 import customFormSectionsReducer from "../../../Reducers/CustomFormSectionsReducer";
+import Button from "@material-ui/core/Button";
 
 const FormWrapper = styled.div`
   padding-top: 70px;
@@ -19,7 +20,7 @@ function CreateSubmissionForm() {
     customFormSectionsReducer,
     []
   );
-  const [activeSection, setActiveSection] = useState(0);
+  const [page, setPage] = useState(-1);
   const [loadForm, refetch] = usePromise(FORM.getForm, {
     programId: appUser.currentProgram
   });
@@ -41,37 +42,36 @@ function CreateSubmissionForm() {
     });
   }, [loadForm, appUser, refetch]);
 
-  function updateActiveSection(sectionKey) {
-    window.requestAnimationFrame(() => {
-      const element = document.getElementById("section_" + sectionKey);
-      if (element) {
-        element.scrollIntoView({
-          behavior: "smooth",
-          block: "start"
-        });
-      }
-    });
-    if (activeSection !== sectionKey) {
-      setActiveSection(sectionKey);
-    }
-  }
-
   return (
     <div>
-      <CreateEditFormHeader {...headerData} />
-      {sections &&
-        sections.map((section, key) => (
-          <FormWrapper key={key} id={"section_" + key}>
-            <FormSection
-              key={key + "_section"}
-              numSections={sections.length}
-              sectionNum={key + 1}
-              sectionData={section}
-              updateActiveSection={updateActiveSection}
-              active={activeSection === key}
-            />
-          </FormWrapper>
-        ))}
+      {page === -1 ? (
+        <CreateEditFormHeader {...headerData} />
+      ) : sections && page < sections.length ? (
+        <FormWrapper key={page} id={"section_" + page}>
+          <FormSection
+            key={page + "_section"}
+            numSections={sections.length}
+            sectionNum={page + 1}
+            sectionData={sections[page]}
+          />
+        </FormWrapper>
+      ) : null}
+      <Button
+        variant="contained"
+        disabled={page === -1}
+        color="primary"
+        onClick={() => setPage(page - 1)}
+      >
+        Previous
+      </Button>
+      <Button
+        variant="contained"
+        disabled={page === sections.length - 1}
+        color="primary"
+        onClick={() => setPage(page + 1)}
+      >
+        Next
+      </Button>
     </div>
   );
 }
