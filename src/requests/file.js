7e614503f0831cd01bc4b2a08ccb7dd10f2f39b0
Greg Maxin <gmaxin@uwaterloo.ca>
2020-12-06 09:49:13 -0500

    Updated endpoints

--- src/requests/file.js
+++ src/requests/file.js
@@ -1,9 +1,35 @@
 import { GET, POST, PATCH, DELETE } from "./Helper.js";
 
-async function listBuckets() {
-  return GET("/api/files/listBuckets");
-}
+//Getting a file will be done by using the bucket URL and the bucket + filename
+//Ex: arbitrium.s3.us-east-2.amazonaws.com/bucketname/filename.png
+
+//File access must be setup in AWS so our frontend prod app can download the file
 
 async function listBuckets() {
   return GET("/api/files/listBuckets");
 }
+
+async function listFilesofBucket(bucketname) {
+  return GET(`/api/files/listfiles/${bucketname}`);
+}
+
+//Used when creating a program. Each program will have its own bucket
+async function createBucket(bucketname) {
+  return POST(`/api/files/createBucket/${bucketname}`, {});
+}
+
+async function deleteBucket(bucketname) {
+  return POST(`/api/files/deleteBucket/${bucketname}`);
+}
+
+async function fileUpload(bucketname, filename) {
+  //todo
+}
+
+export {
+  listBuckets,
+  listFilesofBucket,
+  createBucket,
+  deleteBucket,
+  fileUpload
+};
