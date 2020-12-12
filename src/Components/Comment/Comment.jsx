8bc021164cab4799004451e312555b9087de5063
Emily Louie <emilyslouie@gmail.com>
2020-12-12 12:11:49 -0500

    remove comment

--- src/Components/Comment/Comment.jsx
+++ src/Components/Comment/Comment.jsx
@@ -61,7 +61,6 @@ const Comment = ({ comment }) => (
           .substring(4, 16)}
       </span>
     </div>
-    {/* <p className="comment-text">{comment.value}</p> */}
     <p className="comment-text">
       {comment.value.split("\n").map((item, key) => {
         return (
