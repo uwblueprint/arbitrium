0414ec374b8f4f8e80900f6686359812ed1e797a
Greg Maxin <gmaxin@uwaterloo.ca>
2021-01-24 23:56:39 -0500

    lint

--- src/Components/List/ApplicationList/AllApplicationsTable.jsx
+++ src/Components/List/ApplicationList/AllApplicationsTable.jsx
@@ -1,7 +1,8 @@
 import React from "react";
 import styled from "styled-components";
 import MaterialTable from "material-table";
-import { Paper, Select, MenuItem, InputLabel } from "@material-ui/core";
+//import { Paper, Select, MenuItem, InputLabel } from "@material-ui/core";
+import { Paper } from "@material-ui/core";
 import TableIcons from "../../Common/TableIcons";
 import moment from "moment";
 
