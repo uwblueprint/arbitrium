d23049370bff75d6b52641480f0a3428a29aae60
ahmedhamodi <ahmed.r.hamodi@gmail.com>
2021-02-07 22:32:40 -0500

    Initial work for csv export of submission/app data.

--- src/Components/ApplicationTable/AllApplicationsTable.jsx
+++ src/Components/ApplicationTable/AllApplicationsTable.jsx
@@ -1,10 +1,17 @@
-import React from "react";
+import React, { useRef, useContext, useState } from "react";
 import styled from "styled-components";
 import MaterialTable from "material-table";
-//import { Paper, Select, MenuItem, InputLabel } from "@material-ui/core";
-import { Paper } from "@material-ui/core";
+import { Paper, Select, MenuItem, InputLabel } from "@material-ui/core";
 import TableIcons from "../Common/TableIcons";
+import GetApp from "@material-ui/icons/GetApp";
+import IconButton from "@material-ui/core/IconButton";
 import moment from "moment";
+import { CSVLink } from "react-csv";
+import { ProgramContext } from "../../Contexts/ProgramContext";
+
+const HiddenCSVLink = styled(CSVLink)`
+  display: none;
+`;
 
 const Container = styled(Paper)`
   table {
@@ -27,28 +34,39 @@ const rowStyle = {
   border: "1px solid #cccccc"
 };
 
-//TODO: Uncomment for CSV
-// const ExportWrapper = styled.div`
-//   .dropdown-text {
-//     color: #2261ad;
-//     text-align: center;
-//     font-size: 14px;
-//     letter-spacing: 1.25px;
-//     position: absolute;
-//     height: 16px;
-//     right: 42px;
-//   }
-//   .dropdown {
-//     width: 180px;
-//     position: absolute;
-//     top: 0%;
-//     bottom: 0%;
-//     left: 0%;
-//     right: 0%;
-//   }
-// `;
+const ExportWrapper = styled.div`
+  .dropdown-text {
+    color: #2261ad;
+    text-align: center;
+    font-size: 14px;
+    letter-spacing: 1.25px;
+    position: absolute;
+    height: 16px;
+    right: 42px;
+  }
+  .dropdown {
+    width: 180px;
+    position: absolute;
+    top: 0%;
+    bottom: 0%;
+    left: 0%;
+    right: 0%;
+  }
+`;
 
-function AllApplicationsTable({ applicationCount, reviewCount, ...props }) {
+function AllApplicationsTable({
+  reviewCount,
+  applicationCount,
+  data,
+  ...props
+}) {
+  const appsDownloadLink = useRef();
+  const applicationsCSVFilename = `Applications - ${moment().format(
+    "DD-MM-YYYY hh-mm-ss"
+  )}.csv`;
+  const programContext = useContext(ProgramContext);
+  const [downloadType, setDownloadType] = useState("csv");
+
   const columns = [
     { title: "Applicant Name", field: "applicantName" },
     { title: "Rating (/5)", field: "rating" },
@@ -65,18 +83,24 @@ function AllApplicationsTable({ applicationCount, reviewCount, ...props }) {
       }
     },
     {
-      title: null,
-      //(
-      //   <>
-      //     <ExportWrapper>
-      //       <InputLabel className="dropdown-text"></InputLabel>
-      //       <Select value="csv" onChange={() => console.info("replace me")}>
-      //         <MenuItem value={"csv"}>Download as CSV</MenuItem>
-      //         <MenuItem value={"pdf"}>Download as PDF</MenuItem>
-      //       </Select>
-      //     </ExportWrapper>
-      //   </>
-      // ),
+      title: (
+        <>
+          <ExportWrapper>
+            <InputLabel className="dropdown-text"></InputLabel>
+            <Select value={"csv"}>
+              <MenuItem value={"csv"} onSelect={() => setDownloadType("csv")}>
+                Download as CSV
+              </MenuItem>
+              <MenuItem value={"pdf"} onSelect={() => setDownloadType("pdf")}>
+                Download as PDF
+              </MenuItem>
+            </Select>
+            <IconButton onClick={exportAllData} size="small">
+              <GetApp />
+            </IconButton>
+          </ExportWrapper>
+        </>
+      ),
       field: "applicantLink",
       sorting: false,
       searchable: false,
@@ -91,8 +115,21 @@ function AllApplicationsTable({ applicationCount, reviewCount, ...props }) {
     showTitle: true
   };
 
+  function exportAllData() {
+    if (downloadType === "csv") {
+      appsDownloadLink.current.link.click();
+    } else if (downloadType === "pdf") {
+      // TODO: export pdf logic
+    }
+  }
+
   return (
     <Wrapper>
+      <HiddenCSVLink
+        ref={appsDownloadLink}
+        filename={applicationsCSVFilename}
+        data={programContext.applications.map(({ _id, ...item }) => item)}
+      />
       <MaterialTable
         key={reviewCount + applicationCount}
         icons={TableIcons}
@@ -100,6 +137,7 @@ function AllApplicationsTable({ applicationCount, reviewCount, ...props }) {
           Container: (props) => <Container {...props} elevation={0} />
         }}
         columns={columns}
+        data={data}
         {...props}
         title={
           (Number.isInteger(reviewCount) ? reviewCount : 0) +
